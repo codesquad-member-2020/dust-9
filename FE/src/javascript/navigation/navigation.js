a5ef14c91445baf3bb7a721b6299e5231801f8d9
Angelo-Valrentin <valrentin.angelo@gmail.com>
2020-04-02 12:35:56 +0900

    [FEAT] 화면전환을 위한 기능 추가
    Title >
    * 화면전환을 위한 기능 추가
    
    Contents >
    * 탭 버튼 클릭에 의한 화면 전환 기능 추가

--- FE/src/javascript/navigation/navigation.js
+++ FE/src/javascript/navigation/navigation.js
@@ -1,9 +1,17 @@
-const init = () => {
-    
+const init = (touchCallbackHandler) => {
+    registerEventListener(touchCallbackHandler);
 }
 
-const registerEventListener = () => {
+const registerEventListener = (touchCallbackHandler) => {
+    const buttons = document.querySelectorAll(".navigation button");
 
+    buttons.forEach((button, index) => {
+        button.addEventListener('touchend', evt => {
+            touchCallbackHandler(index);
+        })
+    });
 }
+
+
 
 export default {init, registerEventListener}
